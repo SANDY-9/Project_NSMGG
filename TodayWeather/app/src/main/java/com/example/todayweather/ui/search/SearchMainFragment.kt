bfc571891f620ba7354bac42a0246e5c725cca24
SANDY <72983926+SANDY-9@users.noreply.github.com>
2021-02-08 23:11:27 +0900

    dd

--- TodayWeather/app/src/main/java/com/example/todayweather/ui/search/SearchMainFragment.kt
+++ TodayWeather/app/src/main/java/com/example/todayweather/ui/search/SearchMainFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import com.example.todayweather.R
 import com.example.todayweather.databinding.FragmentSearchMainBinding
 
 class SearchMainFragment : Fragment() {
 
     lateinit var binding : FragmentSearchMainBinding
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +24,14 @@ class SearchMainFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate<FragmentSearchMainBinding>(inflater, R.layout.fragment_search_main, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        binding.before.setOnClickListener {
+            navController.navigate(R.id.action_searchMainFragment_to_mainFragment)
+        }
     }
 
 }
--- TodayWeather/app/src/main/java/com/example/todayweather/ui/setting/SettingMainFragment.kt
+++ TodayWeather/app/src/main/java/com/example/todayweather/ui/setting/SettingMainFragment.kt
@@ -15,47 +15,17 @@ import com.example.todayweather.ui.main.MainActivity
 class SettingMainFragment : Fragment() {
 
     lateinit var binding : FragmentSettingMainBinding
-    lateinit var activity: MainActivity
     lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(
-            inflater,
-            R.layout.fragment_setting_main,
-            container,
-            false
-        )
-        initView()
-        initDataBinding()
-        initAfterBinding()
-        return binding.root
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_setting_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        activity.setSupportActionBar(binding.toolbar)
-        activity.getSupportActionBar()?.title = "설정"
-        activity.getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
-        if(item.itemId == android.R.id.home) {
-        }
-    }
-    fun initView() {
 
     }
-
-    fun initDataBinding() {
-        activity = getActivity() as MainActivity
-    }
-
-    fun initAfterBinding() {
-        TODO("Not yet implemented")
-    }
-
 }
