3a385695f53f2cac2684bdbbcc0d0c389bd4eaf6
SANDY <72983926+SANDY-9@users.noreply.github.com>
2021-02-09 00:23:48 +0900

    d

--- TodayWeather/app/src/main/java/com/example/todayweather/ui/setting/SettingMainFragment.kt
+++ TodayWeather/app/src/main/java/com/example/todayweather/ui/setting/SettingMainFragment.kt
@@ -1,13 +1,17 @@
 package com.example.todayweather.ui.setting
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.todayweather.R
 import com.example.todayweather.databinding.FragmentSettingMainBinding
 import com.example.todayweather.ui.main.MainActivity
@@ -27,6 +31,9 @@ class SettingMainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        navController = Navigation.findNavController(view)
+        binding.before.setOnClickListener {
+            navController.navigate(R.id.action_settingMainFragment_to_mainFragment)
+        }
     }
 }
